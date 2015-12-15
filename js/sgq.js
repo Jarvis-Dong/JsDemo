8ee66c22b56bf09483a4282544c5ffb5e7a70f2a
jarvis <jarvis.dong1992@gmail.com>
2015-12-15 15:53:17 +0800

    historic answer works score calculate works

--- js/sgq.js
+++ js/sgq.js
@@ -36,12 +36,43 @@ var allQuestions = [
 var questionNum = allQuestions.length;
 var eachQuestionMark = fullScore / questionNum;
 var consoleHtml = function () {
+
     for (var i = 0; i < allQuestions.length; i++) {
         var template = "<div><span>" + allQuestions[i].question + "</span><span>" + allQuestions[i].correctAnswer + "</span><span>" + allQuestions[i].choices + "</span><span style='color: red'>" + allQuestions[i].userChoice + "</span></div>"
         $("#console").append(template);
     }
 };
+function checkform() {
+    if (count > 10) {
+        var radios = $('#radio-container>input');
+        console.log(radios.length);
+        for (var i = 0; i < radios.length; i++) {
+            if (radios[i].checked == true) {
+            }
+            else {
+                return false;
+            }
+        }
+        ;
+        console.log("check了");
+    }
+    else {
+        change();
+        console.log("没check");
+        return true;
+    }
+    ;
 
+};
+function consoleScore() {
+    userScore = 0;
+    //遍历之后计算成绩
+    for (var i = 0; i < allQuestions.length; i++) {
+        if (allQuestions[i].correctAnswer == allQuestions[i].userChoice) {
+            userScore += eachQuestionMark;
+        }
+    }
+}
 function change() {
     if (count >= allQuestions.length) {
         var userChoice = 99;
@@ -53,16 +84,15 @@ function change() {
                 if (allQuestions[count - 1].userChoice == allQuestions[count - 1].correctAnswer) {
                     console.log("true");
                 }
-
             }
-
         }
         ;
+        consoleScore();
         console.log(userScore);
         $("#question").html("");
         $("#radio-container").html("<p>您的成绩是：" + userScore + "</p>")
-
     }
+
     else {
         var userChoice = 99;
         $("#question").html(allQuestions[count].question);
@@ -71,14 +101,12 @@ function change() {
         for (var j = 0; j < radios.length; j++) {
             if (radios[j].checked) {
                 userChoice = radios[j].value;
-                allQuestions[count - 1].userChoice = userChoice;
-                if (allQuestions[count - 1].userChoice == allQuestions[count - 1] //这边的count为什么要-1 点击change 之后应该先做完if语句再走count++啊
+                allQuestions[count-1].userChoice = userChoice;
+                if (allQuestions[count-1].userChoice == allQuestions[count-1] //这边的count为什么要-1 点击change 之后应该先做完if语句再走count++啊
                         .correctAnswer) {
                     console.log("true");
                 }
-
             }
-
         }
         ;
         $("#radio-container").html("");
@@ -86,7 +114,10 @@ function change() {
             var template = "<div class='radio'><label><input class='optionRadios' type='radio' name='optionsRadios' id='optionRadios" + i + "'value='" + i + "'>" + choices[i] + "</label></div>"
             $("#radio-container").append(template);
 
-        };
+        }
+        ;
+        var historyUserChoice = allQuestions[count].userChoice;
+        $("input[name='optionsRadios'][value=" + historyUserChoice + "]").attr("checked", true);
 
     }
     consoleHtml();
@@ -95,38 +126,33 @@ function last() {
     var userChoice = 99;
     $("#question").html(allQuestions[count].question);
     var choices = allQuestions[count].choices;
-    var radios = $(".optionRadios");
-    for (var j = 0; j < radios.length; j++) {
-        if (radios[j].checked) {
-            userChoice = radios[j].value;
-            allQuestions[count - 1].userChoice = userChoice;
-            if (allQuestions[count - 1].userChoice == allQuestions[count - 1] //这边的count为什么要-1 点击change 之后应该先做完if语句再走count++啊
-                    .correctAnswer) {
-                console.log("true");
-            }
-
-        }
-
-    };
     $("#radio-container").html("");
     for (var i = 0; i < choices.length; i++) {
         var template = "<div class='radio'><label><input class='optionRadios' type='radio' name='optionsRadios' id='optionRadios" + i + "'value='" + i + "'>" + choices[i] + "</label></div>"
         $("#radio-container").append(template);
 
-    };
-    //$("input[name='optionsRadios'][value=2]").attr("checked",true);
+    }
+    ;
+    var historyUserChoice = allQuestions[count].userChoice;
+    $("input[name='optionsRadios'][value=" + historyUserChoice + "]").attr("checked", true);
 };
 $(function () {
     $("#next").on("click", function () {
-        count++;
-        change();
+        
+            count++;
+            change();
 
-        console.log(count);
-    })
+
+
+    });
     $("#last").on("click", function () {
-        count--;
-        last();
-        console.log(count);
+        if (count > 0) {
+            count--;
+            last();
+        } else {
+            alert("这已经是第一页了");
+        }
+
     })
 });
 
